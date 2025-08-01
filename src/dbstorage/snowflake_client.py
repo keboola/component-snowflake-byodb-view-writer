dfd6b265deefd86614a2d94e50fe7d0ead2dae69
Albert Sedlacek <albert.sedlacek@keboola.com>
2025-08-01 14:45:09 +0200

    switch to only NoneType check

--- src/dbstorage/snowflake_client.py
+++ src/dbstorage/snowflake_client.py
@@ -190,7 +190,7 @@ class SnowflakeClient:
         result = self.execute_query(query)
         logging.debug(f"Validating schema existence with query: {query}")
 
-        if len(result) == 0 or result is None:
+        if result is None:
             raise UserException(f"Schema {schema} does not exist in database {database}.")
 
     @validate_sql_placeholders
